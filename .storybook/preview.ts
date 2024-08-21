ea3b5e05b0249f39124d418e3556e6a0cdc2c6b8
ShriD2003 <shrithan@wednesday.is>
2024-08-21 13:03:44 +0530

    refactor: add intl config for storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,12 @@
+import { translationMessages, appLocales, DEFAULT_LOCALE } from '../app/i18n';
+
 import type { Preview } from '@storybook/react';
+
+type TranslationMessages = {
+  [key: string]: { [key: string]: string };
+};
+
+const getMessages = (locale: string) => (translationMessages as TranslationMessages)[locale];
 
 const preview: Preview = {
   parameters: {
@@ -7,6 +15,11 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/i
       }
+    },
+    intl: {
+      locales: appLocales,
+      defaultLocale: DEFAULT_LOCALE,
+      getMessages
     }
   }
 };
